98a5ed5e075a7f077e162b4e90a09980c2a8dc87
Catarina <catarinakamp@icloud.com>
2024-02-27 14:28:09 -0300

    Initial Commit

---
+++ lePetitChef-challengeZero/lePetitChef_challengeZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  lePetitChef_challengeZeroApp.swift
+//  lePetitChef-challengeZero
+//
+//  Created by Catarina Liberato on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct lePetitChef_challengeZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
