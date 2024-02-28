1413a816e087fe34f6eb467984cbdc7cbc427155
Catarina <catarinakamp@icloud.com>
2024-02-27 23:34:13 -0300

    Changes

---
+++ lePetitChef-challengeZero/Views/Page Elements/EstimatedTime.swift
@@ -0,0 +1,34 @@
+//
+//  EstimatedTime.swift
+//  lePetitChef-challengeZero
+//
+//  Created by Catarina Liberato on 27/02/24.
+//
+
+import SwiftUI
+
+struct EstimatedTime: View {
+    var recipe: Recipe
+    var body: some View {
+        
+        Text("Tempo previsto")
+            .font(Font
+                .custom("LT Panneaux", size: 24)
+                .weight(.heavy))
+            .foregroundColor(.black)
+            .padding()//diminuir espaço
+        
+        Image(systemName: "clock")
+            .padding()//diminuir espaço
+        
+        Text(recipe.estimatedTime)
+            .font(Font
+                .custom("LT Panneaux", size: 20))
+            .foregroundColor(.black)
+        
+    }
+}
+
+#Preview {
+    EstimatedTime(recipe: Recipe(title: "", estimatedTime: "120 minutos", ingredients: [""], steps: [Steps(numeration: "", title: "", description: "")]))
+}
