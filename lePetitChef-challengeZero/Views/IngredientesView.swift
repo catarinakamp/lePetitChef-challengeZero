ce5ab63167a1e2c0c0c8c9865905d71de65c3bc1
Catarina <catarinakamp@icloud.com>
2024-02-27 22:23:40 -0300

    Ingredientes e tempo

---
+++ lePetitChef-challengeZero/Views/IngredientesView.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientesView.swift
+//  lePetitChef-challengeZero
+//
+//  Created by Catarina Liberato on 27/02/24.
+//
+
+import SwiftUI
+
+struct IngredientesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientesView()
+}
---
+++ lePetitChef-challengeZero/Views/TempoEstimadoView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  lePetitChef-challengeZero
+//
+//  Created by Catarina Liberato on 27/02/24.
+//
+
+import SwiftUI
+
+struct TempoEstimadoView: View {
+    var body: some View {
+        VStack{
+            Text("Tempo estimado")
+            
+            Text()
+
+        }
+    }
+}
+
+#Preview {
+    TempoEstimadoView()
+}
