698adeaf825e3b1c521b869f01f8706a7217a21a
Catarina <catarinakamp@icloud.com>
2024-02-29 17:47:25 -0300

    Pop up corrigido

--- lePetitChef-challengeZero/Views/Page Elements/InfoPopUp.swift
+++ lePetitChef-challengeZero/Views/Page Elements/InfoPopUp.swift
@@ -23,37 +23,43 @@ struct InfoPopUp: View {
             BlurView(style: .systemUltraThinMaterialDark)
                 .ignoresSafeArea()
             
-            ZStack{
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.linguineOrange)
+                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                .frame(width: 350, height: 300)
+            
+            Image("remy-teste")
+                .resizable()
+                .scaledToFit()
+                .position(CGPoint(x: 300, y: 500))
+                .frame(height: 400)
+            
+            VStack {
                 
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.linguineOrange)
-                    .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                Spacer()
                 
-                VStack {
+                Text("Bem-vindo ao Le Petit Chef! Nosso app lhe oferece receitas selecionadas pelo chef Remy. Com passo a passo detalhado e vídeos dos movimentos ensinados por Remy, você terá tudo para criar pratos deliciosos. Explore nossa galeria e cozinhe como um verdadeiro chef!"
+                )
+                .font(Font
+                    .custom("LT Panneaux", size: 20))
+                .padding()
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.white)
+                
+                Button(action: {visibility = .notVisible}, label: {
                     
-                    Spacer()
-                    
-                    Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?")
+                    Text("Fechar")
                         .font(Font
                             .custom("LT Panneaux", size: 20))
-                        .padding()
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.white)
                     
-                    Button(action: {visibility = .notVisible}, label: {
-                        
-                        Text("Fechar")
-                            .foregroundStyle(.white)
-                        
-                    })
-                    
-                    Spacer()
-                    
-                }
+                })
                 
-            }
-            .frame(width: 350, height: 600)
-            
+                Spacer()
+            }            .frame(width: 350, height: 600)
         }
-        
     }
 }
 
