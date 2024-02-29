2d873efb53dd81fd89aad13fbb40542bf8352f95
Catarina <catarinakamp@icloud.com>
2024-02-29 16:37:42 -0300

    InfoPopUp butões erro

--- lePetitChef-challengeZero/Views/Pages/HomePageView.swift
+++ lePetitChef-challengeZero/Views/Pages/HomePageView.swift
@@ -14,49 +14,7 @@ struct HomePageView: View {
         ZStack {
             VStack{
                 
-                //HeaderHomePageView(), precisei colar tudo aqui provisóriamente porque o botão não funcionada de outra forma
-                
-                HStack { //HeaderHomePageView()
-                    
-                    VStack (alignment: .leading, spacing: -2) { //HeaderHomePageView()
-                        
-                        Text("Bonjour") //HeaderHomePageView()
-                            .font(Font //HeaderHomePageView()
-                                .custom("LT Panneaux", size: 32) //HeaderHomePageView()
-                                .weight(.heavy)) //HeaderHomePageView()
-                            .foregroundColor(.black) //HeaderHomePageView()
-                        
-                        HStack{ //HeaderHomePageView()
-                            
-                            Text("Linguine").font(Font //HeaderHomePageView()
-                                .custom("LT Panneaux", size: 32)
-                                .weight(.heavy)) //HeaderHomePageView()
-                            .foregroundColor(.linguineOrange) //HeaderHomePageView()
-                            
-                            Text("!").font(Font //HeaderHomePageView()
-                                .custom("LT Panneaux", size: 32) //HeaderHomePageView()
-                                .weight(.heavy)) //HeaderHomePageView()
-                            .foregroundColor(.black) //HeaderHomePageView()
-                            
-                        }
-                        
-                        Text("Explore receitas").font(Font //HeaderHomePageView()
-                            .custom("LT Panneaux", size: 32) //HeaderHomePageView()
-                            .weight(.heavy)) //HeaderHomePageView()
-                        .foregroundColor(.black) //HeaderHomePageView()
-                        
-                    } //HeaderHomePageView()
-                    
-                    Button(action: { //HeaderHomePageView()
-                        visibility = .isVisible //HeaderHomePageView()
-                    }, label: { //HeaderHomePageView()
-                        Image(systemName: "info.circle") //HeaderHomePageView()
-                            .foregroundStyle(.black) //HeaderHomePageView()
-                        
-                    }) //HeaderHomePageView()
-                } //HeaderHomePageView()
-                
-                //HeaderHomePageView()
+                HeaderHomePageView(visibility: visibility)
                 
                 ScrollView{
                     
@@ -81,45 +39,7 @@ struct HomePageView: View {
             
             if visibility == .isVisible {
                 
-                //InfoPopUp(), precisei colar tudo aqui provisóriamente porque o botão não funcionada de outra forma
-                
-                ZStack{ //InfoPopUp()
-                    
-                    BlurView(style: .systemUltraThinMaterialDark)
-                    ZStack {
-                        
-                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/) //InfoPopUp()
-                            .foregroundColor(.linguineOrange) //InfoPopUp()
-                            .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/) //InfoPopUp()
-                        
-                        VStack { //InfoPopUp()
-                            
-                            Spacer() //InfoPopUp()
-                            
-                            Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?") //InfoPopUp()
-                                .foregroundStyle(.white)
-                                .font(Font //InfoPopUp()
-                                    .custom("LT Panneaux", size: 20)) //InfoPopUp()
-                                .padding()
-                                .frame(width: 330, alignment: .center)//InfoPopUp()
-                            
-                            Button(action: {visibility = .notVisible}, label: { //InfoPopUp()
-                                
-                                Text("Fechar") //InfoPopUp()
-                                    .foregroundStyle(.white) //InfoPopUp()
-                                
-                            }) //InfoPopUp()
-                            
-                            Spacer()//InfoPopUp()
-                            
-                        } //InfoPopUp()
-                        
-                    }
-                    .frame(width: 350, height: 600)//InfoPopUp()
-                }
-                .ignoresSafeArea()
-                
-                //InfoPopUp(), precisei colar tudo aqui provisóriamente porque o botão não funcionada de outra forma
+                InfoPopUp(visibility: visibility)
                 
                 
             }
