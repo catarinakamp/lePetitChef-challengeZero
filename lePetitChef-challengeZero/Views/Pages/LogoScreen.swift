f78620c393f04e7da9f34ef43f8eb6120845e123
Catarina <catarinakamp@icloud.com>
2024-03-04 17:12:21 -0300

    Start

---
+++ lePetitChef-challengeZero/Views/Pages/LogoScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LogoScreen.swift
+//  lePetitChef-challengeZero
+//
+//  Created by Catarina Liberato on 04/03/24.
+//
+
+import SwiftUI
+
+struct LogoScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoScreen()
+}
---
+++ lePetitChef-challengeZero/Views/Pages/RemyTips.swift
@@ -0,0 +1,18 @@
+//
+//  RemyTips.swift
+//  lePetitChef-challengeZero
+//
+//  Created by Catarina Liberato on 04/03/24.
+//
+
+import SwiftUI
+
+struct RemyTips: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemyTips()
+}
